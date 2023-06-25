ba45b6814858b060cff5b0ea6d8c02f0358cf3d9
Donni <donnigundala@gmail.com>
2023-06-26 05:21:53 +0700

    chore: update app config

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -4,6 +4,10 @@ const appConfig = {
   env: process.env.NODE_ENV,
   server: {
     port: process.env.PORT || 3000
+  },
+  winston: {
+    logFormat: process.env.LOG_FORMAT || 'dev',
+    logDir: '../../logs'
   }
 }
 
